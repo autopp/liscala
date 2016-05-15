d502fcad4d2d26c8b676cc6f87eb779973ccd2cf
autopp <autopp.inc@gmail.com>
2016-05-15 23:00:14 +0900

    fix test case

--- src/test/scala/com/autopp/lisp/LispTest.scala
+++ src/test/scala/com/autopp/lisp/LispTest.scala
@@ -8,44 +8,44 @@ class LispTest extends FunSpec {
   describe("Lisp#eval") {
     describe("with (+ 41 1)") {
       it ("returns 42") {
-        lisp.eval("(+ 41 1)") should be(Num(42))
+        lisp.eval("(+ 41 1)") should be(Right(Num(42)))
       }
     }
 
     describe("with (cons 1 2)") {
       it ("returns (1 . 2)") {
-        lisp.eval("(cons 1 2)") should be(Pair(Num(1), Num(2)))
+        lisp.eval("(cons 1 2)") should be(Right(Pair(Num(1), Num(2))))
       }
     }
 
     describe("with (quote x)") {
       it ("returns 'x") {
-        lisp.eval("(quote x)") should be(Sym("a"))
+        lisp.eval("(quote x)") should be(Right(Sym("a")))
       }
     }
 
     describe("with (if #t 1 2)") {
       it ("returns 1") {
-        lisp.eval("(if #t 1 2)") should be(Num(1))
+        lisp.eval("(if #t 1 2)") should be(Right(Num(1)))
       }
     }
 
 
     describe("with (if #f 1 2)") {
       it ("returns 1") {
-        lisp.eval("(if #t 1 2)") should be(Num(2))
+        lisp.eval("(if #t 1 2)") should be(Right(Num(2)))
       }
     }
 
     describe("with (if #f 1)") {
       it ("returns nil") {
-        lisp.eval("(if #f 1)") should be(NilVal)
+        lisp.eval("(if #f 1)") should be(Right(NilVal))
       }
     }
 
     describe("with ((lambda (x y) (+ x y)) 41 1)") {
       it ("returns 41") {
-        lisp.eval("((lambda (x y) (+ x y)) 41 1)") should be(Num(42))
+        lisp.eval("((lambda (x y) (+ x y)) 41 1)") should be(Right(Num(42)))
       }
     }
   }

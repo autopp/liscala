812b7c5b7223d660ce4488ef7ab741c2d3a99f28
autopp <autopp.inc@gmail.com>
2016-05-15 20:13:22 +0900

    imple lookup function

--- src/main/scala/com/autopp/lisp/Env.scala
+++ src/main/scala/com/autopp/lisp/Env.scala
@@ -2,5 +2,16 @@ package com.autopp.lisp
 
 import scala.collection.mutable.Map
 
-class Env {
+class Env(val map: Map[String, SExpr], val prev: Option[Env]) {
+  def lookup(name: String): Option[SExpr] = {
+    map.get(name) match {
+      case Some(sexpr) => Some(sexpr)
+      case None => {
+        prev match {
+          case Some(prevEnv) => prevEnv.lookup(name)
+          case None => None
+        }
+      }
+    }
+  }
 }

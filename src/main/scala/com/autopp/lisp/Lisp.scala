47fd3e7f1707a161266314d2c40c1647d97877d1
autopp <autopp.inc@gmail.com>
2016-05-15 20:57:08 +0900

    impl main routine

--- src/main/scala/com/autopp/lisp/Lisp.scala
+++ src/main/scala/com/autopp/lisp/Lisp.scala
@@ -1,9 +1,24 @@
 package com.autopp.lisp
 
 class Lisp {
-  def eval(source: String): SExpr = NilVal
+  def eval(source: String): Lisp.Result = Right(NilVal)
 }
 
 object Lisp {
   type Result = Either[String, SExpr]
+
+  def main(args: Array[String]) {
+    val lisp = new Lisp
+    for (str <- args) {
+      lisp.eval(str) match {
+        case Left(msg) => {
+          System.err.println(s"ERROR: ${msg}")
+          sys.exit(1)
+        }
+        case Right(sexpr) => {
+          println(sexpr.toString)
+        }
+      }
+    }
+  }
 }
